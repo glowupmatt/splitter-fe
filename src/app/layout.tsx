4951c3e500c7087ca8dc49dfc25473d79d2d3619
glowupmatt <glowupmatt@gmail.com>
2025-04-04 23:41:24 -0700

    Refactor layout and Home components to use AudioControlsProvider and streamline audio handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Audio_controls_context from "@/context/audio_controls_context";
+import AudioControlsProvider from "@/context/Audio_controls_context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Audio_controls_context>{children}</Audio_controls_context>
+        <AudioControlsProvider>{children}</AudioControlsProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,11 @@ import Information_card from "@/components/custom_components/Information_card";
 import information_data from "@/shared_data/information_data";
 import description_data from "@/shared_data/description_data";
 import Upload_input from "@/components/custom_components/Upload_input";
-import { SplitAudioResponse } from "@/types/split_audio_types";
-import { useState } from "react";
-import { response_test } from "../shared_data/testing_data";
+import { useAudioControls } from "@/context/Audio_controls_context";
+import { AudioPlayer } from "@/components/custom_components/waveForm/AudioPlayer";
 
 export default function Home() {
-  const [response, setResponse] = useState<SplitAudioResponse>(response_test);
-
-  const audio_links = Object.keys(response_test.downloads).map((key) => {
-    return {
-      key: key,
-      value: response.downloads[key as keyof typeof response.downloads],
-    };
-  });
+  const { response } = useAudioControls();
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-black to-blue-950">
@@ -40,19 +32,7 @@ export default function Home() {
           })}
         </div>
         <div className="w-full p-4 border-2 border-dashed rounded-lg">
-          <button className="text-white">Play All</button>
-          {response ? (
-            audio_links.map((audio) => {
-              const { key } = audio;
-              return (
-                <div key={key} className="flex flex-col gap-4">
-                  Display
-                </div>
-              );
-            })
-          ) : (
-            <Upload_input setResponse={setResponse} />
-          )}
+          {response?.downloads ? <AudioPlayer /> : <Upload_input />}
         </div>
       </div>
     </div>
